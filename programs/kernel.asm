05dec84136af2c7a547408ce09d851176053eb2d
Or <thiccckkque@gmail.com>
2026-04-05 13:43:13 +0300

    have a semi functioning shell that display character input

--- programs/kernel.asm
+++ programs/kernel.asm
@@ -1,5 +1,5 @@
 ; -----------------------------------------
-; SECTOR 1: THE OS KERNEL (VGA FONT BLITTER)
+; SECTOR 1: THE OS KERNEL (THE SHELL)
 ; -----------------------------------------
 .DEFINE DIR_RAM 8192
 .DEFINE FONT_RAM 10000    
@@ -13,6 +13,10 @@ ROW_CNT: 0
 COL_CNT: 0
 ROW_VAL: 0
 
+; The Shell Cursor
+CURSOR_X: 20
+CURSOR_Y: 20
+
 .TEXT
 JMP OS_MAIN
 
@@ -21,7 +25,6 @@ JMP OS_MAIN
 ; R1 = X, R2 = Y, R3 = ASCII Char, R4 = Color
 ; -----------------------------------------
 DRAW_CHAR:
-    ; 1. Calculate Sprite Memory Address = FONT_RAM + (Char * 8)
     LOD R3
     PSH 8
     MUL
@@ -30,30 +33,28 @@ DRAW_CHAR:
     PUT CHAR_PTR
     DRP
 
-    MOV ROW_CNT, 0  ; Initialize row counter to 0
+    MOV ROW_CNT, 0
 CHAR_ROW_LOOP:
     MOV R0, 8
     CMP ROW_CNT, R0
     JIE CHAR_END
 
-    ; 2. Load the 8-bit Sprite Row
     LOD CHAR_PTR
     LOD ROW_CNT
     ADD
     PUT R0          
     DRP
     
-    LDB R0          ; Read the 8 pixels
-    PUT ROW_VAL     ; Store sprite byte
+    LDB R0
+    PUT ROW_VAL
     DRP
 
-    MOV COL_CNT, 0  ; Initialize col counter to 0
+    MOV COL_CNT, 0
 CHAR_COL_LOOP:
     MOV R0, 8
     CMP COL_CNT, R0
     JIE CHAR_COL_END
 
-    ; 3. Bitwise Magic: Check if pixel is 1
     LOD ROW_VAL
     PSH 7
     LOD COL_CNT
@@ -64,14 +65,12 @@ CHAR_COL_LOOP:
     PUT R0
     DRP
 
-    ; If bit is 0, skip drawing
-    MOV RS, 1       ; Use RS as a temp register to hold 1
+    MOV RS, 1
     CMP R0, RS
     JIE DRAW_PIXEL
     JMP SKIP_PIXEL
 
 DRAW_PIXEL:
-    ; 4. Calculate VRAM Address = VRAM + (Y + ROW_CNT) * 320 + (X + COL_CNT)
     LOD R2
     LOD ROW_CNT
     ADD
@@ -86,13 +85,11 @@ DRAW_PIXEL:
     PUT R0
     DRP
 
-    ; 5. Blast the Color to VRAM
     LOD R4
     STB R0
     DRP
 
 SKIP_PIXEL:
-    ; col_counter++
     LOD COL_CNT
     PSH 1
     ADD
@@ -101,7 +98,6 @@ SKIP_PIXEL:
     JMP CHAR_COL_LOOP
 
 CHAR_COL_END:
-    ; row_counter++
     LOD ROW_CNT
     PSH 1
     ADD
@@ -116,38 +112,69 @@ CHAR_END:
 ; MAIN OS BOOT
 ; -----------------------------------------
 OS_MAIN:
-    ; 1. Clear Screen
+    ; 1. Clear Screen & Load File System
     MOV RS, CLEAR
     SYS
-
-    ; 2. Load the Directory (Sector 9)
     MOV RS, DISK_READ
     MOV RX, 9
     MOV RY, DIR_RAM
     MOV RC, 1
     SYS
 
-    ; 3. Load font.bin (Sector 10)
+    ; 2. Load font.bin (Sector 10)
     MOV RS, DISK_READ
     MOV RX, 10
     MOV RY, FONT_RAM
     MOV RC, 4       
     SYS
 
-    ; 4. Print "OS" using our custom Software Blitter!
-    MOV R1, 150     ; X = 150
-    MOV R2, 90      ; Y = 90
-    MOV R3, 79      ; ASCII 79 = 'O'
-    MOV R4, 115     ; Color = Sky Blue
+    ; 3. Draw the Command Line Prompt ">" (ASCII 62)
+    MOV R1, 10
+    MOV R2, 20
+    MOV R3, 62
+    MOV R4, 42      ; Cyan color for the prompt
     RUN DRAW_CHAR
 
-    MOV R1, 160     ; X = 160 (Shift right by 10 pixels)
-    MOV R2, 90      ; Y = 90
-    MOV R3, 83      ; ASCII 83 = 'S'
-    MOV R4, 115     ; Color = Sky Blue
+; -----------------------------------------
+; THE TYPEWRITER LOOP
+; -----------------------------------------
+TYPEWRITER:
+    ; A. Clear RC before we poll for input (crucial!)
+    MOV RC, 0
+    
+    ; B. Ask Hardware for a keystroke
+    MOV RS, INPUT
+    SYS
+
+    ; C. If RC is 0, no key was pressed. Skip to rendering!
+    MOV R0, 0
+    CMP RC, R0
+    JIE RENDER_FRAME
+
+    ; D. A KEY WAS PRESSED! Draw it!
+    LOD CURSOR_X
+    PUT R1
+    DRP
+
+    LOD CURSOR_Y
+    PUT R2
+    DRP
+
+    LOD RC          ; RC holds the ASCII value of the key pressed!
+    PUT R3
+    DRP
+
+    MOV R4, 115     ; Sky blue text
     RUN DRAW_CHAR
 
-IDLE:
+    ; E. Advance the Cursor 10 pixels to the right
+    LOD CURSOR_X
+    PSH 10
+    ADD
+    PUT CURSOR_X
+    DRP
+
+RENDER_FRAME:
     MOV RS, RENDER
     SYS
-    JMP IDLE
+    JMP TYPEWRITER
